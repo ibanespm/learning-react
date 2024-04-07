3ac824ef33b2c924ea16087bdf2a591ab5111d40
ibanespm <ibanesp59@gmail.com>
2024-04-07 18:11:30 -0500

    add: init learning

--- react-courses/src/index.js
+++ react-courses/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+function Greeting(){
+        return<div>
+            <h1>HELLO WORLD - THIS IS A COMPONET</h1>
+            <p>lorem 123</p>
+        </div>
+}
+
+root.render(<div>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+
+</div>)
+
+// root.render(
+//     <div>
+//         <Greeting />
+//     </div>
